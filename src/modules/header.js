b4f8a2055d6a7ee9f3531fe82059ae161d10e016
Melissa Christiana <machristiana@ufl.edu>
2021-08-02 10:27:07 -0400

    Create module folder and files

---
+++ src/modules/header.js
@@ -0,0 +1,10 @@
+export class Header {
+    constructor(pageTitle, pageLogoSrc) {
+        this.pageTitle = pageTitle;
+        this.pageLogoSrc = pageLogoSrc;
+    }
+}
+
+// export const headerPageTitle = new Header('Easy 30 Minute Lunches')
+
+// export const headerPageLogoSrc = new Header('images/logo5.png')
